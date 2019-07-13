52a2ab4a31ec9a39e6340055588b4b15e37ac1c2
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2019-07-13 12:11:52 +0700

    install and add firebase-admin

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('./src/db/mongoose')
-
-const { ErrorResponse } = require('@model/HTTPResponse')
-
-const authenticate = require('@middleware/authenticate')
+const admin = require('firebase-admin')
 
 const Authentication = require('@router/Authentication')
 const Member = require('@router/Member')
 const Post = require('@router/Post')
 const Feed = require('@router/Feed')
 
+const authenticate = require('@middleware/authenticate')
+
+const { ErrorResponse } = require('@model/HTTPResponse')
+
 const port = process.env.PORT || 3000
 
 let app = express()
 
+const serviceAccount = require("./lovesick-react-firebase-adminsdk-vndcv-1c91db8e2f.json");
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://lovesick-react.firebaseio.com"
+});
+
 app.use(bodyParser.json())
 
 app.get('/', authenticate, (req, res) => {
-    res.send(`Hello World ${req.user.username}`)
+	res.send(`Hello World ${req.user.username}`)
 });
 
 app.use('/auth', Authentication)
