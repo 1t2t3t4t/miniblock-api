182e2e621daa3c7f8bc5caa7264cc8d00c3ecdca
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2019-07-13 12:55:57 +0700

    delete comment

--- test/middleware/authenticate.test.js
+++ test/middleware/authenticate.test.js
@@ -117,27 +117,4 @@ describe('The middleware ensures that request has a valid token before perform a
         authenticate(req, res, next)
     })
 })
-//
-//
-// const authenticate = (req, res, next) => {
-//     const token = req.headers.authorization
-//
-//     if (!token) throw Error('Require Authorization.')
-//
-//     admin.auth().verifyIdToken(token)
-//         .then((decodedToken) => User.findByUID(decodedToken.uid))
-//         .then((user) => {
-//             if (!user) {
-//                 throw Error('User is null or not found.')
-//             }
-//
-//             req.user = user
-//             req.token = token
-//             next()
-//         })
-//         .catch((e) => {
-//             res.status(401)
-//             next(e)
-//         })
-// }
 
