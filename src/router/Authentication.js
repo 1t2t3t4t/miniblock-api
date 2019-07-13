1f3c3d0bf4d6a22a313c5e527512ff3a4b12c688
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2019-07-13 23:08:23 +0700

    Add authentication docs

--- src/router/Authentication.js
+++ src/router/Authentication.js
@@ -4,6 +4,21 @@ const { ErrorResponse, Response } = require('@model/HTTPResponse')
 
 const route = express.Router()
 
+/**
+ * @api {POST} /auth/login Login
+ * @apiDeprecated Dont really know if we need this or not so keep it just in case.
+ * @apiName Register
+ * @apiGroup Authentication
+ *
+ * @apiParam {String} uid           Users unique ID.
+ * @apiParam {String} email         Users email.
+ * @apiParam {String} username      Username.
+ *
+ * @apiSuccess {String} message     Successfully message.
+ * @apiSuccess {User}   User        User model.
+ *
+ * @apiError {Error} InternalError error with message
+ */
 route.post('/login', (req, res) => {
     const email = req.body.email
     const password = req.body.password
@@ -18,6 +33,21 @@ route.post('/login', (req, res) => {
     })
 })
 
+/**
+ * @api {POST} /auth/register Register
+ * @apiDescription Register user from Firebase to the database **Must be called**
+ * @apiName Register
+ * @apiGroup Authentication
+ *
+ * @apiParam {String} uid           Users unique ID.
+ * @apiParam {String} email         Users email.
+ * @apiParam {String} username      Username.
+ *
+ * @apiSuccess {String} message     Successfully message.
+ * @apiSuccess {User}   User        User model.
+ *
+ * @apiError {Error} InternalError error with message
+ */
 route.post('/register', async (req, res) => {
     const { email, username, uid } = req.body
 
