22d832d4ddbb703964e4d59b6dca8b0dcc13a49c
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2019-07-24 23:08:05 +0700

    add search tests

--- test/endpoint/feed.test.ts
+++ test/endpoint/feed.test.ts
@@ -193,8 +193,119 @@ describe('Fetch all from feed with mixed category', () => {
                 assert.notDeepEqual(res.body.body.posts, undefined)
                 const resPosts: Array<PostModel> = res.body.body.posts
                 assert.deepEqual(resPosts.length, 10)
+                const expectId = posts
+                    .filter((post) => post.categoryId == categoryId)
+                    .find((post, idx) => idx == 10)!._id.toString()
+                assert.deepEqual(expectId, resPosts[0]._id)
                 resPosts.forEach((post) => {
                     assert.deepEqual(post.categoryId, categoryId)
+                })
+            }).end(done)
+    })
+
+})
+
+describe('Seach post', () => {
+
+    const dbManager = new DBManager()
+    let posts: Array<PostModel> = []
+
+    const keyword = 'keyword'
+    before((next) => {
+
+        const makePost = (title: string, creator: string): PostModel => {
+            return {
+                type: PostType.TEXT,
+                categoryId: Category.Loneliness,
+                content: {
+                    text: `text`
+                },
+                title: title,
+                creator: creator,
+            } as PostModel
+        }
+
+        const stubPost = async (creator: string) => {
+            let postList: PostModel[] = []
+            postList.push(makePost(keyword, creator))
+            postList.push(makePost('random post', creator))
+            postList.push(makePost(`${keyword} followed my smt`, creator))
+            postList.push(makePost('random post 2', creator))
+            postList.push(makePost(`lead by smt ${keyword} followed my smt`, creator))
+            postList.push(makePost('random post 3', creator))
+            postList.push(makePost(`lllll${keyword}llll`, creator))
+            postList.push(makePost('random post 3', creator))
+            postList.push(makePost(`hey yo ${keyword}`, creator))
+            for(const post of postList) {
+                const returnedPost = await new Post(post).save()
+                posts.push(returnedPost)
+            }
+        }
+
+        dbManager.start().then(() => {
+            return User.findByUID("1")
+        }).then((user: UserModel) => {
+            return stubPost(user._id)
+        }).then(() => {
+            next()
+        }).catch((e: Error) => {
+            console.log(e)
+        })
+    })
+
+    after(() => {
+        dbManager.stop()
+    })
+
+    const path = '/v1/feed/search'
+    it('should get all post that matches keyword', (done) => {
+        const postWithKeyword = posts.reduce((tot, curr) => {
+            return tot += curr.title.includes(keyword) ? 1 : 0
+        }, 0)
+        request(app)
+            .get(path + '?keyword=' + keyword)
+            .expect(200)
+            .expect((res: Response) => {
+                assert.notDeepEqual(res.body.body.posts, undefined)
+                const resPosts: Array<PostModel> = res.body.body.posts
+                assert.deepEqual(postWithKeyword, resPosts.length)
+                resPosts.forEach((post) => {
+                    assert(post.title.includes(keyword), 'should contain keyword')
+                })
+            }).end(done)
+    })
+
+    it('should get all post that matches keyword with limit', (done) => {
+        request(app)
+            .get(path + '?keyword=' + keyword + '&limit=3')
+            .expect(200)
+            .expect((res: Response) => {
+                assert.notDeepEqual(res.body.body.posts, undefined)
+                const resPosts: Array<PostModel> = res.body.body.posts
+                assert.deepEqual(3, resPosts.length)
+                resPosts.forEach((post) => {
+                    assert(post.title.includes(keyword), 'should contain keyword')
+                })
+            }).end(done)
+    })
+
+    it('should get all post that matches keyword with limit and afterId', (done) => {
+        const thirdPost = posts
+            .filter((post) => post.title.includes(keyword))
+            .find((post, idx) => idx == 2)!._id
+        request(app)
+            .get(path + '?keyword=' + keyword + '&limit=3&afterId=' + thirdPost)
+            .expect(200)
+            .expect((res: Response) => {
+                assert.notDeepEqual(res.body.body.posts, undefined)
+                const resPosts: Array<PostModel> = res.body.body.posts
+                const expectId = posts
+                    .filter((post) => post.title.includes(keyword))
+                    .find((post, idx) => idx == 3)!._id.toString()
+                assert.deepEqual(resPosts[0]._id, expectId)
+                assert.deepEqual(2, resPosts.length)
+                resPosts.forEach((post) => {
+                    assert(post.title.includes(keyword), 'should contain keyword')
                 })
             }).end(done)
     })
