a7268c37782552545ea0d1ffd4afc9f5c0873c73
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2019-08-26 22:23:54 +0700

    update docs

--- src/v1/Chat/ChatRouterController.ts
+++ src/v1/Chat/ChatRouterController.ts
@@ -11,6 +11,17 @@ export default class ChatRouterController {
 
     chatRoomDAO = new ChatRoomDAO()
 
+    /**
+     * @api {GET} /v1/chats Get chats
+     * @apiDescription Get all of user's chats
+     * @apiGroup Chat
+     * @apiPermission loggedIn
+     *
+     * @apiHeader {String} Authorization Token string from Firebase
+     *
+     * @apiSuccess {ChatRoom[]} chatRooms ChatRoom array model
+     *
+     * */
     @GET('/')
     @Middleware(ensureAuthenticate)
     all(req: FeedQueryRequest, res: express.Response, next: express.NextFunction) {
